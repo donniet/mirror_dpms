e685d8db492e6be35cea283affc6d4d03fb261c5
Donnie Tolbert <donniet@gmail.com>
2023-01-28 17:58:24 -0600

    switched to XLib

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
-#include <dbus/dbus.h>
+#include <X11/Xlib.h>
+#include <X11/extensions/dpms.h>
 
 int main(int ac, char * av[]) {
-    DBusError err;
-    dbus_error_init(&err);
+    // Open a connection to the X server
+    Display* dpy = XOpenDisplay(nullptr);
 
-    DBusConnection *connection = dbus_bus_get(DBUS_BUS_SYSTEM, &err);
-    if (dbus_error_is_set(&err)) {
-        std::cerr << "Failed to connect to system bus: " << err.message << std::endl;
-        return -1;
+    // Get the current DPMS state
+    BOOL on;
+    CARD16 state;
+    DPMSInfo(dpy, &state, &on);
+
+    if (on) {
+        // Turn DPMS off
+        DPMSDisable(dpy);
+    } else {
+        // Turn DPMS on
+        DPMSEnable(dpy);
     }
 
-    DBusMessage* msg = dbus_message_new_method_call("org.freedesktop.UPower", 
-                                                   "/org/freedesktop/UPower", 
-                                                   "org.freedesktop.UPower", 
-                                                   "Resume");
-
-    if(!msg) {
-        std::cerr << "Failed to create message" << std::endl;
-        return -1;
-    }
-
-    dbus_connection_send(connection, msg, NULL);
-    dbus_connection_flush(connection);
-    dbus_message_unref(msg);
-    dbus_connection_unref(connection);
+    // Close the connection to the X server
+    XCloseDisplay(dpy);
 
     return 0;
 }
